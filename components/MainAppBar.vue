3f983f2cb539ce5818f8a1cb76015f02d3d109c3
jonathan <jonnyschoeman@gmail.com>
2022-03-23 16:02:25 +0200

    Working on main app bar looks and functionality

---
+++ components/MainAppBar.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <!--  Navigation Drawer Start  -->
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  <!--  App Bar Start  -->
+    <v-app-bar :clipped-left="clipped" fixed app elevate-on-scroll>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn class="mr-1 font-weight-bold" color="#FBC02D" depressed rounded small outlined>
+        Products
+        <v-icon>mdi-script-text-outline</v-icon>
+      </v-btn>
+      <v-btn color="#2E7D32" class="mr-1 lighten-3" dark depressed rounded small to="/">
+        Documents
+        <v-icon class="ml-1">mdi-folder-open-outline</v-icon>
+      </v-btn>
+      <v-btn color="#2E7D32" class="mr-1 lighten-3" dark depressed rounded small to="/">
+        Latest News
+        <v-icon class="ml-1">mdi-book-open-outline</v-icon>
+      </v-btn>
+      <v-btn color="#2E7D32" class="lighten-3" dark depressed rounded small to="/">
+        About Us
+        <v-icon class="ml-1">mdi-nature-people</v-icon>
+      </v-btn>
+      <v-btn color="green" class="ml-5" fab dark depressed small>
+        <v-icon>mdi-whatsapp</v-icon>
+      </v-btn>
+      <v-btn color="green" class="ml-1" fab dark depressed small>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainAppBar",
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'e-Milling',
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <MainAppBar />
     <v-main>
       <v-container>
         <Nuxt />
@@ -57,7 +17,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; {{ new Date().getFullYear() }} e-Milling</span>
     </v-footer>
   </v-app>
 </template>
@@ -69,7 +29,7 @@ export default {
     return {
       clipped: false,
       drawer: false,
-      fixed: false,
+      fixed: true,
       items: [
         {
           icon: 'mdi-apps',
@@ -90,3 +50,7 @@ export default {
   },
 }
 </script>
+
+<style>
+
+</style>
